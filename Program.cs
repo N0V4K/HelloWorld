e6e2995846e1d687c94d7043a985f8447e2937f9
N0V4K <33703477+N0V4K@users.noreply.github.com>
2017-12-05 15:07:11 -0700

    Update4

--- Program.cs
+++ Program.cs
@@ -9,7 +9,27 @@ namespace HelloWorld
             Console.WriteLine("Write 2 numbers");
             float number1 = float.Parse(Console.ReadLine());
             float number2 = float.Parse(Console.ReadLine());
-            Console.WriteLine(number1 - number2);
+
+            Console.Write("Select an option:\n1.-Add\n2.-Subtract\n3.-Multiply\n4.-Divide");
+
+            int option = int.Parse(Console.ReadLine());
+
+
+            EvaluateOption(option, number1, number2);
+
+            Console.WriteLine(AddTwoNumbers(number1, number2));
         }
+        public static float AddTwoNumbers(float n1, float n2)
+        {
+            float result = n1 + n2;
+
+            return result;
+        }
+  
+        public static void EvaluateOption(int option, float n1, float n2)
+        {
+            if (option == 1)
+            {
+                Console.WriteLine("Result of addition: " + AddTwoNumbers(n1,n2));
     }
 }
